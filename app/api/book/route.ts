4459eb4277c647a5a82018c81b3d7ba133c2e4e9
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2025-07-11 01:38:51 -0300

    :sparkles: feat: get book user

--- app/api/book/route.ts
+++ app/api/book/route.ts
@@ -31,3 +31,40 @@ export async function POST(req: NextRequest) {
 
   return NextResponse.json({ book }, { status: 201 });
 }
+
+export async function GET(req: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user.email) {
+      return NextResponse.json({ error: "Não autorizado", status: 401 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Usuário não encontrado" });
+    }
+
+    const books = await prisma.book.findMany({
+      where: {
+        userId: user.id,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return NextResponse.json({ books }, { status: 200 });
+  } catch (error) {
+    console.log("[ERROR] Erro ao buscar livros:", error);
+    return NextResponse.json(
+      { error: "Erro interno no servidor" },
+      { status: 500 }
+    );
+  }
+}
